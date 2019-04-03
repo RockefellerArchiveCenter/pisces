dfe5458804ff13697c36948d8e2342b86b7d9c1b
Hillel Arnold <helrond@hotmail.com>
2019-04-03 08:54:32 -0400

    working on travis

--- pisces/settings.py
+++ pisces/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from . import config as CF
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,15 +78,7 @@ WSGI_APPLICATION = 'pisces.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
+DATABASES = CF.DATABASES
 
 
 # Password validation
