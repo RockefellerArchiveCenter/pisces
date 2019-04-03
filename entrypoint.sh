257b402eb78c2fcf758c7004d4f7a468723e57bb
Hillel Arnold <helrond@hotmail.com>
2019-04-03 13:00:40 -0400

    adds check for config file, fixes #2

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@
 echo "Apply database migrations"
 ./wait-for-it.sh db:5432 -- python manage.py migrate
 
+if [ ! -f pisces/config.py ]; then
+    cp pisces/config.py.example pisces/config.py
+fi
+
 #Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
