1e52272c54849aff625f8a9246942e6f7b34a1c9
p-galligan <pjg250@gmail.com>
2019-04-09 11:35:47 -0400

    Update data_fetchers.py
    agent and term logic

--- transformer/data_fetchers.py
+++ transformer/data_fetchers.py
@@ -1,3 +1,23 @@
 
 
 # Add data fetchers here
+
+#LOGIC
+#Get all archival objects updated in the last hour
+    #For each updated archival object
+        #Open and read the JSON
+            #With JSON Open
+                #Iterate through all linked agents and append their ids to a list
+                    #Check to see if id exists in database
+                        #if exists
+                            #do nothing
+                        #if doesn't exists
+                            #get agent JSON based on its ID
+                            #save JSON data for transformation and placement into DB
+                #Iterate through all linked terms and append their ids to a list
+                    #Check to see if id exists in database
+                        #if exists
+                            #do nothing
+                        #if doesn't exists
+                            #get term JSON based on its ID
+                            #save JSON data for transformation and placement into DB
