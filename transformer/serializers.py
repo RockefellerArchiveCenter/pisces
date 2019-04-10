2466222641584161bd8d13a9412345d1c9deac0c
Hillel Arnold <helrond@hotmail.com>
2019-04-10 18:04:08 -0400

    builds out serializers

--- transformer/serializers.py
+++ transformer/serializers.py
@@ -1,11 +1,77 @@
 from rest_framework import serializers
-from transformer.models import Collection, Object, Agent, Term
+from transformer.models import *
+
+
+class DateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Date
+        fields = ("begin", "end", "expression", "label")
+
+
+class ExtentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Extent
+        fields = ('value', 'type')
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = ('expression', 'identifier')
+
+
+class NoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Note
+        fields = ("type", "title", "content")
+
+
+class RightsGrantedSerializer(serializers.ModelSerializer):
+    note = NoteSerializer(source="note_set", many=True)
+
+    class Meta:
+        model = RightsGranted
+        fields = ("act", "restriction", "dateStart", "dateEnd", "note")
+
+
+class RightsStatementSerializer(serializers.ModelSerializer):
+    rights_granted = RightsGrantedSerializer(source='rightsgranted_set', many=True)
+    note = NoteSerializer(source="note_set", many=True)
+
+    class Meta:
+        model = RightsStatement
+        fields = ("rightsType", "dateStart", "dateEnd", "determinationDate",
+                  "rights_granted", "copyrightStatus", "otherBasis", "jurisdiction",
+                  "note", "created", "modified")
+
+
+class IdentifierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Identifier
+        fields = ("source", "identifier", "created", "modified")
+
+
+class SourceDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SourceData
+        fields = ('source', 'data')
 
 
 class CollectionSerializer(serializers.HyperlinkedModelSerializer):
+    languages = LanguageSerializer(many=True)
+    dates = DateSerializer(source="date_set", many=True)
+    extents = ExtentSerializer(source="extent_set", many=True)
+    notes = NoteSerializer(source="note_set", many=True)
+    rights_statements = RightsStatementSerializer(source="rightsstatement_set", many=True)
+    identifiers = IdentifierSerializer(source="identifier_set", many=True)
+    source_data = SourceDataSerializer(source="sourcedata_set", many=True)
+
     class Meta:
         model = Collection
-        fields = '__all__'
+        fields = ("url", "title", "dates", "creators", "languages", "notes",
+                  "extents", "level", "agents", "terms", "parents", "rights_statements",
+                  "identifiers", "source_data", "created", "modified", )
 
 
 class CollectionListSerializer(serializers.HyperlinkedModelSerializer):
@@ -15,9 +81,19 @@ class CollectionListSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class ObjectSerializer(serializers.HyperlinkedModelSerializer):
+    languages = LanguageSerializer(many=True)
+    dates = DateSerializer(source="date_set", many=True)
+    extents = ExtentSerializer(source="extent_set", many=True)
+    notes = NoteSerializer(source="note_set", many=True)
+    rights_statements = RightsStatementSerializer(source="rightsstatement_set", many=True)
+    identifiers = IdentifierSerializer(source="identifier_set", many=True)
+    source_data = SourceDataSerializer(source="sourcedata_set", many=True)
+
     class Meta:
         model = Object
-        fields = '__all__'
+        fields = ("url", "title", "dates", "languages", "notes", "extents",
+                  "agents", "terms", "parents", "identifiers", "rights_statements",
+                  "source_data", "created", "modified")
 
 
 class ObjectListSerializer(serializers.HyperlinkedModelSerializer):
@@ -27,9 +103,15 @@ class ObjectListSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class AgentSerializer(serializers.HyperlinkedModelSerializer):
+    dates = DateSerializer(source="date_set", many=True)
+    notes = NoteSerializer(source="note_set", many=True)
+    identifiers = IdentifierSerializer(source="identifier_set", many=True)
+    source_data = SourceDataSerializer(source="sourcedata_set", many=True)
+
     class Meta:
         model = Agent
-        fields = '__all__'
+        fields = ("url", "title", "dates", "type", "notes", "identifiers",
+                  "source_data", "created", "modified")
 
 
 class AgentListSerializer(serializers.HyperlinkedModelSerializer):
@@ -39,9 +121,14 @@ class AgentListSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class TermSerializer(serializers.HyperlinkedModelSerializer):
+    notes = NoteSerializer(source="note_set", many=True)
+    identifiers = IdentifierSerializer(source="identifier_set", many=True)
+    source_data = SourceDataSerializer(source="sourcedata_set", many=True)
+
     class Meta:
         model = Term
-        fields = '__all__'
+        fields = ("url", "title", "type", "notes", "identifiers", "source_data",
+                  "created", "modified")
 
 
 class TermListSerializer(serializers.HyperlinkedModelSerializer):
