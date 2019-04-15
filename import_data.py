606b2641774b23cc1881bf6680ad24ffc7007f5d
Hillel Arnold <helrond@hotmail.com>
2019-04-15 13:35:28 -0400

    ignore if identifier exists

--- import_data.py
+++ import_data.py
@@ -13,33 +13,60 @@ TYPE_MAP = {'agent_corporate_entity': [Agent, 'agent'],
             'subjects': [Term, 'term']}
 
 for d in os.listdir(os.path.join(settings.BASE_DIR, source_filepath)):
-    if (d not in ['trees', 'maps']) and isinstance(d, dir):
+    # Handle data from ArchivesSpace
+    if (d not in ['trees', 'maps']) and os.path.isdir(os.path.join(settings.BASE_DIR, source_filepath, d)):
         cls = TYPE_MAP[d][0]
         key = TYPE_MAP[d][1]
         for f in os.listdir(os.path.join(settings.BASE_DIR, source_filepath, d)):
-            with open(os.path.join(settings.BASE_DIR, source_filepath, d, f)) as f:
-                data = json.load(f)
-                if d == 'archival_objects':
-                    resource_id = data.get('resource').get('ref').split('/')[-1]
-                    with open(os.path.join(settings.BASE_DIR, source_filepath, 'trees', '{}.json'.format(resource_id))) as tf:
-                        tree_data = json.load(tf)
-                        full_tree = objectpath.Tree(tree_data)
-                        partial_tree = full_tree.execute("$..children[@.record_uri is '{}']".format(data.get('uri')))
-                        # Tree.execute() is a generator function so we have to loop through the results
-                        for p in partial_tree:
-                            if p.get('has_children'):
-                                key = 'collection'
-                                obj = Collection.objects.create(tree=p)
-                            else:
-                                key = 'object'
-                                obj = cls.objects.create()
-                elif d == 'resources':
-                    resource_id = data.get('uri').split('/')[-1]
-                    with open(os.path.join(settings.BASE_DIR, source_filepath, 'trees', '{}.json'.format(resource_id))) as tf:
-                        tree_data = json.load(tf)
-                        obj = cls.objects.create(tree=tree_data)
-                else:
-                    obj = cls.objects.create()
-                SourceData.objects.create(**{key: obj, "source": Identifier.ARCHIVESSPACE, "data": data})
-                Identifier.objects.create(**{key: obj, "source": Identifier.ARCHIVESSPACE, "identifier": data.get('uri')})
-                print("Imported {}".format(data.get('uri')))
+            with open(os.path.join(settings.BASE_DIR, source_filepath, d, f)) as jf:
+                data = json.load(jf)
+                if not Identifier.objects.filter(source=Identifier.ARCHIVESSPACE, identifier=data.get('uri')).exists():
+                    # Handle archival object records
+                    if d == 'archival_objects':
+                        resource_id = data.get('resource').get('ref').split('/')[-1]
+                        # Load tree JSON
+                        with open(os.path.join(settings.BASE_DIR, source_filepath, 'trees', '{}.json'.format(resource_id))) as tf:
+                            tree_data = json.load(tf)
+                            full_tree = objectpath.Tree(tree_data)
+                            partial_tree = full_tree.execute("$..children[@.record_uri is '{}']".format(data.get('uri')))
+                            # Save archival object as Collection if it has children, otherwise save as Object
+                            # Tree.execute() is a generator function so we have to loop through the results
+                            for p in partial_tree:
+                                if p.get('has_children'):
+                                    key = 'collection'
+                                    obj = Collection.objects.create(tree=p)
+                                else:
+                                    key = 'object'
+                                    obj = cls.objects.create()
+                    # Handle resource records
+                    elif d == 'resources':
+                        resource_id = data.get('uri').split('/')[-1]
+                        with open(os.path.join(settings.BASE_DIR, source_filepath, 'trees', '{}.json'.format(resource_id))) as tf:
+                            tree_data = json.load(tf)
+                            obj = cls.objects.create(tree=tree_data)
+                    # Handle agent and term records
+                    else:
+                        obj = cls.objects.create()
+                    # Create SourceData and Identifier objects
+                    SourceData.objects.create(**{key: obj, "source": SourceData.ARCHIVESSPACE, "data": data})
+                    Identifier.objects.create(**{key: obj, "source": Identifier.ARCHIVESSPACE, "identifier": data.get('uri')})
+                    print("Imported {}".format(data.get('uri')))
+    # Handle data from Cartographer
+    elif d == 'maps':
+        for f in os.listdir(os.path.join(settings.BASE_DIR, source_filepath, d)):
+            # Load arrangement map data
+            with open(os.path.join(settings.BASE_DIR, source_filepath, d, f)) as jf:
+                data = json.load(jf)
+                if not Identifier.objects.filter(source=Identifier.CARTOGRAPHER, identifier=data.get('title')).exists():
+                    # Handle top-level collection from arrangement map
+                    c = Collection.objects.create(tree=data.get('children'))
+                    SourceData.objects.create(collection=c, source=SourceData.CARTOGRAPHER, data=data)
+                    Identifier.objects.create(collection=c, source=Identifier.CARTOGRAPHER, identifier=data.get('title')) # TODO: this needs to be updated
+                    print("Imported {}".format(data.get('title')))
+                    # Save collections in tree that don't have refs (ie things that don't come from AS)
+                    for collection in data.get('children'):
+                        if not collection.get('ref'):
+                            c = Collection.objects.create(tree=collection.get('children'))
+                            SourceData.objects.create(collection=c, source=SourceData.CARTOGRAPHER, data=collection)
+                            Identifier.objects.create(collection=c, source=Identifier.CARTOGRAPHER, identifier=collection.get('title')) # TODO: this needs to be updated
+                            print("Imported {}".format(collection.get('title')))
