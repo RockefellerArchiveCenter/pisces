b8ef73bf5a53c1024e51f26484ee149f719579cb
p-galligan <pjg250@gmail.com>
2019-04-19 17:05:04 -0400

    preliminary objects collections
    adding framework for objects and collections. Need to figure out fetching portions now.

--- transformer/data_fetchers.py
+++ transformer/data_fetchers.py
@@ -1,14 +1,14 @@
 # Add data fetchers here
-from dateutil import parser
+#from dateutil import parser
 import os
 import json
 from asnake.aspace import ASpace
-from pycountry import languages
-from uuid import uuid4
+#from pycountry import languages
+#from uuid import uuid4
 
-from django.utils import timezone
+#from django.utils import timezone
 
-from .models import *
+#from .models import *
 
 
 aspace = ASpace(
@@ -21,14 +21,59 @@ repo = aspace.repositories(2)
 terms = []
 
 def check_subjects(self, archival=False, library=False, updated=0):
-        for s in self.repo.subjects.with_params(all_ids=True, modified_since=updated):
+        for s in aspace.subjects:
             if s.publish:
-                terms.append(s['ref'])
+                term_check()
             else:
                 pass
 
+def check_agents():
+        #for a in self.repo.agents.with_params(all_ids=True, modified_since=updated):
+        for a in aspace.agents["people"]:
+            print(a.uri)
+            #if a.publish:
+                #agent_check()
+            #else:
+                #pass
+
+with open(os.path.join(settings.BASE_DIR, source_filepath, 'trees', '{}.json'.format(resource_id))) as tf:
+    tree_data = json.load(tf)
+    full_tree = objectpath.Tree(tree_data)
+    partial_tree = full_tree.execute("$..children[@.record_uri is '{}']".format(data.get('uri')))
+    # Save archival object as Collection if it has children, otherwise save as Object
+    # Tree.execute() is a generator function so we have to loop through the results
+        for p in partial_tree:
+            if p.get('has_children'):
+                pass
+            else:
+                pass
+
+#objects function
+def objects_check():
+    if Identifier.objects.filter(source=Identifier.ARCHIVESSPACE, identifier=p.ref.exists():
+        new_object = Object.objects.filter(source=Identifier.ARCHIVESSPACE, identifier=p.ref)
+        sd = SourceData.objects.get(object=new_object)
+        sd.data = p.json
+        sd.save()
+    else:
+        object = Obect.objects.create(source=Identifier.ARCHIVESSPACE, data=p.json)
+        Identifier.objects.create(object=object, source=Identifier.ARCHIVESSPACE, identifier=p.ref)
+        SourceData.objects.create(object=object, source=Identifier.ARCHIVESSPACE, data=p.json)
+
+#objects function
+def collections_check():
+    if Identifier.objects.filter(source=Identifier.ARCHIVESSPACE, identifier=p.ref.exists():
+        new_object = Collection.objects.filter(source=Identifier.ARCHIVESSPACE, identifier=p.ref)
+        sd = SourceData.objects.get(collection=new_object)
+        sd.data = p.json
+        sd.save()
+    else:
+        collection = Collection.objects.create(source=Identifier.ARCHIVESSPACE, data=p.json)
+        Identifier.objects.create(collection=collection, source=Identifier.ARCHIVESSPACE, identifier=p.ref)
+        SourceData.objects.create(collection=collection, source=Identifier.ARCHIVESSPACE, data=p.json)
+
 #terms function
-def term_check()
+def term_check():
     if Identifier.objects.filter(source=Identifier.ARCHIVESSPACE, identifier=s.ref.exists():
         new_object = Term.objects.filter(source=Identifier.ARCHIVESSPACE, identifier=s.ref)
         sd = SourceData.objects.get(term=new_object)
@@ -40,7 +85,7 @@ def term_check()
         SourceData.objects.create(term=term, source=Identifier.ARCHIVESSPACE, data=s.json)
 
 #agents function
-def agent_check()
+def agent_check():
     if Identifier.objects.filter(source=Identifier.ARCHIVESSPACE, identifier=a.ref.exists():
         new_object = Agent.objects.filter(source=Identifier.ARCHIVESSPACE, identifier=a.ref)
         sd = SourceData.objects.get(agent=new_object)
@@ -50,35 +95,3 @@ def agent_check()
         agent = Agent.objects.create(source=Identifier.ARCHIVESSPACE, data=a.json)
         Identifier.objects.create(agent=agent, source=Identifier.ARCHIVESSPACE, identifier=a.ref)
         SourceData.objects.create(agent=agent, source=Identifier.ARCHIVESSPACE, data=a.json)
-
-#LOGIC
-#Get all archival objects, resources, agents, terms updated in the last hour
-    #For each updated archival object, agent, term, resource
-        #set term.json to a variable
-        #get ref
-            #Check to see if id exists in database
-                #if exists
-                    #get object from database
-                    #set object to new object variable
-                    #get source data
-                    #set source data to nobj.sourcedata_set().filter(source) - this is checking for a matching foreign key in the source data
-                    #set sd.data to term.json (set the source data section to the updated exported json)
-                    #sd.save() save the updated data
-                #if doesn't exists
-                    #create the object
-                    #create Identifier(object=ob)
-                    #create SourceData(object=ob)
-#Separate steps for AO. Tackle in future.
-                #Find ref for the resource (this should be the same as the tree id)
-                #Use that ref to grab the tree JSON
-                #Store the tree data
-                    #With tree data open
-                    #GET CHILDREN & PARENTS
-                    #USE THE TREE JSON to search for the object ID
-                        #When you find the correct ID
-                            #Check for children
-                                #If you find children, store them to a list
-                                #If you don't find children, don't do anything
-                            #Go back up tree to find parents and ancestors
-                                #Store them to a list
-                #Check if object already exists in the database
