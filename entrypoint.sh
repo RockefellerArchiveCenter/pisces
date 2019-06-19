a48c65cd045c69f45b8cb00ee2612845bc7cf51e
Hillel Arnold <helrond@hotmail.com>
2019-06-19 16:18:41 -0400

    adding reference code

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,9 @@ fi
 echo "Apply database migrations"
 python manage.py makemigrations && python manage.py migrate
 
+# python manage.py shell -c "from django.contrib.auth.models import User; \
+#   User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
+
 #Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
