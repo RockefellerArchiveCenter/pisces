faa0118c4ab9ee016be4218db0f4ea067d691bb8
Hillel Arnold <helrond@hotmail.com>
2019-07-14 18:31:29 -0400

    cleaning up and using configs

--- transformer/indexers.py
+++ transformer/indexers.py
@@ -2,21 +2,20 @@ import json
 
 from elasticsearch import Elasticsearch
 
+from pisces import config
+
 
 class Indexer:
 
     def __init__(self):
-        self.client = Elasticsearch(['elasticsearch'])
+        self.client = Elasticsearch([config.ELASTICSEARCH['host']])
 
     def add(self, data):
         if isinstance(data, str):
             data = json.loads(data)
         # TODO: figure out id generation
         # TODO: does this handle updates as well?
-        print(data)
         return self.client.index(index=data.get('type'), doc_type=data.get('type'), body=data)
-        # print("Adding {} to index".format(data))
-        # return "{} added to index".format(data)
 
     def delete(self, data):
         # print("Deleting {} from index".format(data))
