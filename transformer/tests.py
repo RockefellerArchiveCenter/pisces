1b204a1b9a89e722d7185d8a49ca4abbbc1286ea
Hillel Arnold <helrond@hotmail.com>
2019-10-15 17:23:36 -0500

    remove indexing

--- transformer/tests.py
+++ transformer/tests.py
@@ -10,7 +10,6 @@ from django.urls import reverse
 from odin.codecs import json_codec
 
 from .fetchers import *
-from .indexers import *
 from .resources import *
 from .test_library import import_fixture_data, add_wikidata_ids, add_wikipedia_ids, get_random_string
 from .transformers import *
@@ -53,13 +52,3 @@ class TransformerTest(TestCase):
                         self.assertNotEqual(transform, False)
                         valid = validate(instance=json.loads(transform), schema=schema)
                         self.assertEqual(valid, None)
-
-    #def test_indexing(self):
-        #for resource in AS_TYPE_MAP:
-            #for f in os.listdir(os.path.join('fixtures', resource[0])):
-                #with open(os.path.join('fixtures', resource[0], f), 'r') as json_file:
-                    #obj = ArchivesSpaceDataTransformer().run(json.load(json_file))
-                    #add = Indexer().add(obj)
-                    #self.assertNotEqual(add, False)
-                    #delete = Indexer().delete(obj)
-                    #self.assertNotEqual(delete, False)
