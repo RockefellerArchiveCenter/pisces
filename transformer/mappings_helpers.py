74425b5bca2dc2244adcebf2267fff741b4c8316
Hillel Arnold <helrond@hotmail.com>
2020-02-03 13:11:37 -0500

    target correct ASpace attribute

--- transformer/mappings_helpers.py
+++ transformer/mappings_helpers.py
@@ -7,11 +7,11 @@ from pisces import settings
 class ArchivesSpaceHelper:
     def __init__(self):
         self.aspace = ASpace(baseurl=settings.ARCHIVESSPACE['baseurl'],
-                             user=settings.ARCHIVESSPACE['user'],
+                             username=settings.ARCHIVESSPACE['username'],
                              password=settings.ARCHIVESSPACE['password'])
         self.repo = self.aspace.repositories(settings.ARCHIVESSPACE['repo'])
-        if type(self.repo) == dict and 'error' in self.repo:
-            raise ArchivesSpaceDataFetcherError(self.repo['error'])
+        if isinstance(self.repo, dict) and 'error' in self.repo:
+            raise FetcherError(self.repo['error'])
 
     def closest_parent_value(self, uri, key):
         """Iterates up through an archival object's ancestors looking for the
