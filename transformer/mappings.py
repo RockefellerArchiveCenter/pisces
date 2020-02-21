453262e78843616b8d0044183f05c14820179273
Patrick <pgalligan@rockarch.org>
2020-02-21 09:32:21 -0500

    update language maps
    Updates object > collection language mapping to use closest parent function. Also updates the cassettes.

--- transformer/mappings.py
+++ transformer/mappings.py
@@ -223,10 +223,9 @@ class ArchivesSpaceArchivalObjectToCollection(odin.Mapping):
 
     @odin.map_field(from_field='language', to_field='languages', to_list=True)
     def languages(self, value):
-        if value:
-            lang_data = languages.get(part2b=value)
-            return [Language(expression=lang_data.name, identifier=value)]
-        return [Language(expression="English", identifier="eng")]
+        value = value if value else self.aspace_helper.closest_parent_value(self.source.uri, 'language')
+        lang_data = languages.get(part2b=value)
+        return [Language(expression=lang_data.name, identifier=value)]
 
     @odin.map_list_field(from_field='subjects', to_field='terms')
     def terms(self, value):
