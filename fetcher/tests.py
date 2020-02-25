2ad30835f8e1fe646782a02accb62e74f49f583a
Hillel Arnold <helrond@hotmail.com>
2020-02-25 15:24:42 -0500

    add fetcher tests

--- fetcher/tests.py
+++ fetcher/tests.py
@@ -8,15 +8,24 @@ from rest_framework.test import APIRequestFactory
 
 from .fetchers import ArchivesSpaceDataFetcher, CartographerDataFetcher
 from .helpers import last_run_time
-from .models import FetchRun
+from .models import FetchRun, FetchRunError
 from .views import (ArchivesSpaceDeletesView, ArchivesSpaceUpdatesView,
                     CartographerDeletesView, CartographerUpdatesView)
 
-fetch_vcr = vcr.VCR(
+archivesspace_vcr = vcr.VCR(
     serializer='json',
     cassette_library_dir='fixtures/cassettes/fetcher',
     record_mode='once',
     match_on=['path', 'method', 'query'],
+    filter_query_parameters=['username', 'password', 'modified_since'],
+    filter_headers=['Authorization', 'X-ArchivesSpace-Session'],
+)
+
+cartographer_vcr = vcr.VCR(
+    serializer='json',
+    cassette_library_dir='fixtures/cassettes/fetcher',
+    record_mode='once',
+    match_on=['path', 'method'],
     filter_query_parameters=['username', 'password', 'modified_since'],
     filter_headers=['Authorization', 'X-ArchivesSpace-Session'],
 )
@@ -35,40 +44,27 @@ class FetcherTest(TestCase):
             f.start_time = time
             f.save()
 
-    def test_archivesspace_fetcher(self):
-        for status in ["updated", "deleted"]:
-            for object_type in FetchRun.ARCHIVESSPACE_OBJECT_TYPE_CHOICES:
-                with fetch_vcr.use_cassette("ArchivesSpace-{}-{}.json".format(status, object_type[0])):
-                    list = ArchivesSpaceDataFetcher().fetch(status, object_type[0])
-                    for obj in list:
-                        self.assertTrue(isinstance(obj, str))
-        self.assertTrue(len(FetchRun.objects.all()), len(FetchRun.ARCHIVESSPACE_OBJECT_TYPE_CHOICES) * 2)
+    def test_fetchers(self):
+        for object_type_choices, fetcher, fetcher_vcr, cassette_prefix in [
+                (FetchRun.ARCHIVESSPACE_OBJECT_TYPE_CHOICES, ArchivesSpaceDataFetcher, archivesspace_vcr, "ArchivesSpace"),
+                (FetchRun.CARTOGRAPHER_OBJECT_TYPE_CHOICES, CartographerDataFetcher, cartographer_vcr, "Cartographer")]:
+            for status in ["updated", "deleted"]:
+                for object_type in object_type_choices:
+                    with fetcher_vcr.use_cassette("{}-{}-{}.json".format(cassette_prefix, status, object_type[0])):
+                        list = fetcher().fetch(status, object_type[0])
+                        for obj in list:
+                            self.assertTrue(isinstance(obj, str))
+            self.assertTrue(len(FetchRun.objects.all()), len(object_type_choices) * 2)
+            self.assertEqual(len(FetchRunError.objects.all()), 0)
 
-    def test_archivesspace_views(self):
-        for view, status, url_name in [
-                (ArchivesSpaceDeletesView, "deleted", "fetch-archivesspace-deletes"),
-                (ArchivesSpaceUpdatesView, "updated", "fetch-archivesspace-updates")]:
-            for object_type in FetchRun.ARCHIVESSPACE_OBJECT_TYPE_CHOICES:
-                with fetch_vcr.use_cassette("ArchivesSpace-{}-{}.json".format(status, object_type[0])):
-                    request = self.factory.post("{}?object_type={}".format(reverse(url_name), object_type[0]))
-                    response = view().as_view()(request)
-                    self.assertEqual(response.status_code, 200, "Request error: {}".format(response.data))
-
-    def test_cartographer_fetcher(self):
-        for status in ["updated", "deleted"]:
-            for object_type in FetchRun.CARTOGRAPHER_OBJECT_TYPE_CHOICES:
-                with fetch_vcr.use_cassette("Cartographer-{}-{}.json".format(status, object_type[0])):
-                    list = CartographerDataFetcher().fetch(status, object_type[0])
-                    for obj in list:
-                        self.assertTrue(isinstance(obj, str))
-        self.assertTrue(len(FetchRun.objects.all()), len(FetchRun.CARTOGRAPHER_OBJECT_TYPE_CHOICES) * 2)
-
-    def test_cartographer_views(self):
-        for view, status, url_name in [
-                (CartographerDeletesView, "deleted", "fetch-archivesspace-deletes"),
-                (CartographerUpdatesView, "updated", "fetch-archivesspace-updates")]:
-            for object_type in FetchRun.CARTOGRAPHER_OBJECT_TYPE_CHOICES:
-                with fetch_vcr.use_cassette("Cartographer-{}-{}.json".format(status, object_type[0])):
+    def test_views(self):
+        for view, status, url_name, object_type_choices, fetcher_vcr, cassette_prefix in [
+                (ArchivesSpaceDeletesView, "deleted", "fetch-archivesspace-deletes", FetchRun.ARCHIVESSPACE_OBJECT_TYPE_CHOICES, archivesspace_vcr, "ArchivesSpace"),
+                (ArchivesSpaceUpdatesView, "updated", "fetch-archivesspace-updates", FetchRun.ARCHIVESSPACE_OBJECT_TYPE_CHOICES, archivesspace_vcr, "ArchivesSpace"),
+                (CartographerDeletesView, "updated", "fetch-cartographer-deletes", FetchRun.CARTOGRAPHER_OBJECT_TYPE_CHOICES, cartographer_vcr, "Cartographer"),
+                (CartographerUpdatesView, "updated", "fetch-cartographer-updates", FetchRun.CARTOGRAPHER_OBJECT_TYPE_CHOICES, cartographer_vcr, "Cartographer")]:
+            for object_type in object_type_choices:
+                with fetcher_vcr.use_cassette("{}-{}-{}.json".format(cassette_prefix, status, object_type[0])):
                     request = self.factory.post("{}?object_type={}".format(reverse(url_name), object_type[0]))
                     response = view().as_view()(request)
                     self.assertEqual(response.status_code, 200, "Request error: {}".format(response.data))
