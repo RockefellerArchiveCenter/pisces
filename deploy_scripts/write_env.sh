514b6879f8535f6eedaa7fbf13e9d3037f3a15a5
Hillel Arnold <helrond@hotmail.com>
2020-05-06 09:29:28 -0500

    simplify

--- deploy_scripts/write_env.sh
+++ deploy_scripts/write_env.sh
@@ -5,4 +5,4 @@ echo Running `basename "$0"`
 rm env.txt
 
 # Writes environment variables starting with $1 to env.txt, after replacing $1 with $2
-echo "`printenv | grep $1`" | sed "s/${1}/${2}/g" >> env.txt
+echo "`printenv | grep $1`" >> env.txt
