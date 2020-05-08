f052f064acbde72217d2dfca395cedcb20d58b36
Hillel Arnold <helrond@hotmail.com>
2020-05-08 11:58:13 -0500

    removing unnecessary code

--- deploy_scripts/run_management_commands.sh
+++ deploy_scripts/run_management_commands.sh
@@ -4,14 +4,6 @@ ROOT_DIR=/data/app/zodiac/pisces
 
 cd $ROOT_DIR
 
-printenv
-
-source /etc/profile.d/pisces.sh
-
-echo "*** Env variables were sourced here."
-
-printenv
-
 # Run database migrations
 env/bin/python manage.py migrate
 
--- deploy_scripts/write_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm env.txt
-
-# Writes environment variables to env.txt
-echo "`printenv | grep $1`" >> env.txt
