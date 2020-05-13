f4fb33e4ba3b7f1f3f2f31773e52b4bcdb6a15a0
Hillel Arnold <helrond@hotmail.com>
2020-05-13 12:03:33 -0500

    separate out removal of files

---
+++ deploy_scripts/remove_files.sh
@@ -0,0 +1,14 @@
+rm -rf fixtures \
+  .git \
+  .github \
+  .coverage \
+  .flake8 \
+  .gitignore \
+  .pre-commit-config.yaml \
+  cc-test-reporter \
+  docker-compose.yml \
+  Dockerfile \
+  entrypoint.sh \
+  pisces-services.png \
+  wait-for-it.sh \
+  *.pyc \
