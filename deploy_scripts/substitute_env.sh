3106a5e4f381288b83d712b253d48416a7caa162
Hillel Arnold <helrond@hotmail.com>
2020-05-15 23:09:47 -0500

    trying to make this permissions thing work

--- deploy_scripts/substitute_env.sh
+++ deploy_scripts/substitute_env.sh
@@ -2,7 +2,8 @@
 set -e
 
 for TEMPLATE in pisces/config.py.deploy deploy_scripts/install_dependencies.sh.deploy \
-deploy_scripts/run_management_commands.sh.deploy deploy_scripts/validate_service.sh.deploy
+deploy_scripts/run_management_commands.sh.deploy deploy_scripts/validate_service.sh.deploy \
+deploy_scripts/set_permissions.sh.deploy
 do
   if [[ -f "$TEMPLATE" ]]; then
     echo "Replacing variables in $TEMPLATE"
