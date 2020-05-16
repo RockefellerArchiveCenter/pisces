90ba168fe429856ab6ce529b9ab027b9e47d9175
Hillel Arnold <helrond@hotmail.com>
2020-05-16 14:42:19 -0500

    improve variable substitution

--- deploy_scripts/substitute_env.sh
+++ deploy_scripts/substitute_env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-for TEMPLATE in pisces/config.py.deploy deploy_scripts/install_dependencies.sh.deploy \
-deploy_scripts/run_management_commands.sh.deploy deploy_scripts/validate_service.sh.deploy \
-deploy_scripts/set_permissions.sh.deploy
+for TEMPLATE in pisces/config.py.deploy \
+  appspec.yml.deploy
+  deploy_scripts/*.deploy \
 do
   if [[ -f "$TEMPLATE" ]]; then
     echo "Replacing variables in $TEMPLATE"
--- pisces/settings.py
+++ pisces/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = config.DJANGO_STATIC_ROOT
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Django REST Framework
 REST_FRAMEWORK = {
