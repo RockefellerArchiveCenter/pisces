284b5de5ab3fe6099ca3cbb5b2319d6d81cc42cc
Hillel Arnold <helrond@hotmail.com>
2020-05-16 19:09:38 -0500

    send emails with errors at end of fetch

--- fetcher/tests.py
+++ fetcher/tests.py
@@ -1,6 +1,8 @@
+import random
 from datetime import datetime
 
 import vcr
+from django.core import mail
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
@@ -19,7 +21,7 @@ from .cron import (DeletedArchivesSpaceArchivalObjects,
                    UpdatedArchivesSpaceSubjects,
                    UpdatedCartographerArrangementMapComponents)
 from .fetchers import ArchivesSpaceDataFetcher, CartographerDataFetcher
-from .helpers import last_run_time
+from .helpers import last_run_time, send_error_notification
 from .models import FetchRun, FetchRunError
 from .views import (ArchivesSpaceDeletesView, ArchivesSpaceUpdatesView,
                     CartographerDeletesView, CartographerUpdatesView)
@@ -117,3 +119,17 @@ class FetcherTest(TestCase):
                 (cartographer_vcr, "Cartographer-updated-arrangement_map_component.json", UpdatedCartographerArrangementMapComponents)]:
             with fetcher_vcr.use_cassette(cassette):
                 cron().do()
+
+    def test_error_notifications(self):
+        fetch_run = FetchRun.objects.create(
+            object_type=random.choice(FetchRun.OBJECT_TYPE_CHOICES),
+            source=random.choice(FetchRun.SOURCE_CHOICES),
+            status=random.choice(FetchRun.STATUS_CHOICES),
+            object_status=random.choice(FetchRun.OBJECT_STATUS_CHOICES))
+        error = FetchRunError.objects.create(message="This is an error!", run=fetch_run)
+        send_error_notification(fetch_run)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertIn(fetch_run.object_type[1], mail.outbox[0].subject)
+        self.assertIn(fetch_run.source[1], mail.outbox[0].subject)
+        self.assertNotIn("errors", mail.outbox[0].subject)
+        self.assertIn(error.message, mail.outbox[0].body)
