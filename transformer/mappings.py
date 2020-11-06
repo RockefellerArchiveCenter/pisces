00e4ee42c8bf72eae93001fced83784f5392a7a1
Hillel Arnold <helrond@hotmail.com>
2020-11-06 09:01:47 -0500

    remove unnecessary print

--- transformer/mappings.py
+++ transformer/mappings.py
@@ -375,7 +375,6 @@ class SourceArchivalObjectToCollection(odin.Mapping):
         title = value.strip() if value else self.source.display_string.strip()
         if getattr(self.source, "component_id", None):
             title = "{}, {} {}".format(title, self.source.level.capitalize(), self.source.component_id)
-            print(title)
         return title
 
     @odin.map_field(from_field="language", to_field="languages", to_list=True)
